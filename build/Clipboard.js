23d3f78b7197863332b458da81baa734f9ac2e8f
Boyuzhou <2470200492@qq.com>
2018-01-22 16:01:24 +0800

    修改示例

--- build/Clipboard.js
+++ build/Clipboard.js
@@ -32,6 +32,12 @@ var _propTypes = require('prop-types');
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
 
+var _tool = require('bee-locale/build/tool');
+
+var _zhCn = require('./zh-cn');
+
+var _zhCn2 = _interopRequireDefault(_zhCn);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _defaults(obj, defaults) { var keys = Object.getOwnPropertyNames(defaults); for (var i = 0; i < keys.length; i++) { var key = keys[i]; var value = Object.getOwnPropertyDescriptor(defaults, key); if (value && value.configurable && obj[key] === undefined) { Object.defineProperty(obj, key, value); } } return obj; }
@@ -48,36 +54,37 @@ var propTypes = {
     action: _propTypes2["default"].oneOf(['copy', 'cut']),
     text: _propTypes2["default"].string,
     success: _propTypes2["default"].func,
-    error: _propTypes2["default"].func
+    error: _propTypes2["default"].func,
+    locale: _propTypes2["default"].object
 };
 var defaultProps = {
     action: 'copy',
     text: '',
     target: '',
     success: function success() {},
-    error: function error() {}
+    error: function error() {},
+    locale: {}
 };
 
 var Clipboard = function (_Component) {
     _inherits(Clipboard, _Component);
 
-    function Clipboard(props) {
+    function Clipboard(props, context) {
         _classCallCheck(this, Clipboard);
 
-        var _this = _possibleConstructorReturn(this, _Component.call(this, props));
+        var _this = _possibleConstructorReturn(this, _Component.call(this, props, context));
 
         _this.blur = function () {
             _this.setState({
                 currect: false,
-                tootipContent: '复制'
+                ready: false
             });
         };
 
         _this.state = {
             currect: false,
-            actionTitle: '复制',
             html: '',
-            tootipContent: '复制',
+            ready: false,
             id: 'id' + Math.round(Math.random() * 1000 + 1) + new Date().getTime()
         };
         return _this;
@@ -86,20 +93,16 @@ var Clipboard = function (_Component) {
     Clipboard.prototype.componentWillMount = function componentWillMount() {
         var self = this;
         var _props = this.props,
-            action = _props.action,
             success = _props.success,
             error = _props.error;
 
-        var title = action === 'copy' ? '复制' : '剪切';
-        self.setState({
-            actionTitle: title
-        });
+
         var id = this.state.id;
         var cb = new _clipboard2["default"]('#' + id);
         cb.on('success', function (e) {
             self.setState({
                 currect: true,
-                tootipContent: '已复制'
+                ready: true
             });
             e.clearSelection();
             if (success instanceof Function) success();
@@ -114,7 +117,6 @@ var Clipboard = function (_Component) {
     };
 
     Clipboard.prototype.render = function render() {
-        var seft = this;
         var _props2 = this.props,
             action = _props2.action,
             text = _props2.text,
@@ -122,21 +124,35 @@ var Clipboard = function (_Component) {
 
         if (text) action = 'copy';
 
+        var locale = (0, _tool.getComponentLocale)(this.props, this.context, 'Clipboard', function () {
+            return _zhCn2["default"];
+        });
+        var tootipContent = locale[action];
+        if (this.state.ready) {
+            tootipContent = locale[action + 'Ready'];
+        }
+
         return _react2["default"].createElement(
             _beeTooltip2["default"],
-            { positionTop: '20px', overlay: seft.state.tootipContent, placement: 'top' },
+            {
+                positionTop: '20px',
+                overlay: tootipContent,
+                placement: 'top' },
             _react2["default"].createElement(
                 'span',
-                { onMouseOut: this.blur, className: 'u-clipboard', id: this.state.id, 'data-clipboard-action': action,
-                    'data-clipboard-target': target, 'data-clipboard-text': text },
-                this.props.children ? this.props.children : _react2["default"].createElement(
-                    _beeIcon2["default"],
-                    { className: (0, _classnames2["default"])({
-                            'uf-correct': this.state.currect,
-                            'uf-copy': !this.state.currect
-                        }) },
-                    ' '
-                )
+                {
+                    onMouseOut: this.blur,
+                    className: 'u-clipboard',
+                    id: this.state.id,
+                    'data-clipboard-action': action,
+                    'data-clipboard-target': target,
+                    'data-clipboard-text': text },
+                this.props.children ? this.props.children : _react2["default"].createElement(_beeIcon2["default"], {
+                    className: (0, _classnames2["default"])({
+                        'uf-correct': this.state.currect,
+                        'uf-copy': !this.state.currect
+                    })
+                })
             )
         );
     };
---
+++ build/zh-cn.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports["default"] = {
+    lang: 'zh-cn',
+    copy: '复制',
+    cut: '剪切',
+    copyReady: '已复制',
+    cutReady: '已剪切'
+};
+module.exports = exports['default'];
